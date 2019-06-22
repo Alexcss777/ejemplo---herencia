a19d9a91f68a2ddb6648bc08d65c14d8dabde772
Alexcss777 <51940752+Alexcss777@users.noreply.github.com>
2019-06-22 09:29:09 -0700

    Add files via upload

---
+++ herencia/herencia/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            clsClienteventas Cliente;
+            // INTANCIA DE LA CLASE
+            Cliente = new clsClienteventas();
+
+            Cliente.IdCliente = 123;
+            Cliente.Apellidos = "pon atencion";
+            Cliente.Nombres = "Jose Luis";
+            Cliente.RFC = "DSNMFSD";
+            Cliente.Direccion = "Inserte aqui direccion de CESCA";
+            Cliente.Municipio = "Tijuana es un municipio? :/";
+            Cliente.EsCredito = true;
+
+            Console.WriteLine(Cliente.IdCliente+ "  "+Cliente.Apellidos + " " + Cliente.Nombres);
+            Console.WriteLine(Cliente.Direccion + " " + Cliente.Municipio);
+
+
+            if (Cliente.EsCredito)
+                Console.WriteLine("el cliente tiene credito");
+            else
+                Console.WriteLine("el cliente no tiene credito");
+
+
+            Console.ReadKey();
+
+                
+        }
+    }
+}
---
+++ herencia/herencia/clsCliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace herencia
+{
+    public class clsCliente
+    {
+
+        private int _idCliente;
+
+       private string _Nombres;
+
+        private string _Apellidos;
+
+        public int IdCliente { get => _idCliente; set => _idCliente = value; }
+        public string Nombres { get => _Nombres; set => _Nombres = value; }
+        public string Apellidos { get => _Apellidos; set => _Apellidos = value; }
+    }
+}
